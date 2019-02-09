1ce750ebea76d8eb522daad46e50ba8dc9ba8fba
Joakim Nilsson <nijoakim@gmail.com>
2019-02-10 00:07:40 +0100

    Data structures for periodic table (not done).

---
+++ platina/elements.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+# Copyright 2019 Joakim Nilsson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import periodic_table as pt
+
+#==================
+# Add the elements
+#==================
+
+# Period 1
+pt.element('H',  'Hydrogen', 1, 1)
+pt.element('He', 'Helium',   1, 18)
+
+# Period 2
+pt.element('Li', 'Lithium',   2, 1)
+pt.element('Be', 'Beryllium', 2, 2)
+pt.element('B',  'Boron',     2, 13)
+pt.element('C',  'Carbon',    2, 14)
+pt.element('N',  'Nitrogen',  2, 15)
+pt.element('O',  'Oxygen',    2, 16)
+pt.element('F',  'Flourine',  2, 17)
+pt.element('Ne', 'Neon',      2, 18)
+
+# Period 3
+pt.element('Na', 'Sodium',     3, 1)
+pt.element('Mg', 'Magnesium',  3, 2)
+pt.element('Al', 'Aluminium',  3, 13)
+pt.element('Si', 'Silicon',    3, 14)
+pt.element('P',  'Phosphorus', 3, 15)
+pt.element('S',  'Sulfur',     3, 16)
+pt.element('Cl', 'Chlorine',   3, 17)
+pt.element('Ar', 'Argon',      3, 18)
+
+# Period 4
+pt.element('K',  'Potassium', 4, 1)
+pt.element('Ca', 'Calcium',   4, 2)
+pt.element('Sc', 'Scandium',  4, 3)
+pt.element('Ti', 'Titanium',  4, 4)
+pt.element('V',  'Vanadium',  4, 5)
+pt.element('Cr', 'Chromium',  4, 6)
+pt.element('Mn', 'Manganese', 4, 7)
+pt.element('Fe', 'Iron',      4, 8)
+pt.element('Co', 'Cobalt',    4, 9)
+pt.element('Ni', 'Nickel',    4, 10)
+pt.element('Cu', 'Copper',    4, 11)
+pt.element('Zn', 'Zink',      4, 12)
+pt.element('Ga', 'Gallium',   4, 13)
+pt.element('Ge', 'Germanium', 4, 14)
+pt.element('As', 'Arsenic',   4, 15)
+pt.element('Se', 'Selenium',  4, 16)
+pt.element('Br', 'Bromium',   4, 17)
+pt.element('Kr', 'Krypton',   4, 18)
--- platina/periodic_table.py
+++ platina/periodic_table.py
@@ -15,12 +15,40 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import periodic_table as pt
-import elements as e
+# Dictionaries for searching
+symbols = {}
+names   = {}
 
-def main():
-	pt.print_table()
+# 1-indexed periods and groups
+periods = [None]
+for i in range(7):
+	periods.append([None]*19)
 
-# Run main
-if __name__ == "__main__":
-    main()
+def print_table():
+	for period in periods:
+		if period is not None:
+			for element in period[1:]:
+				if element is not None:
+					print(element, ' '*(3-len(str(element))), end='')
+				else:
+					print('    ', end='')
+			print()
+
+class element:
+	def __init__(
+			self,
+			symbol,
+			name,
+			period,
+			group,
+		):
+
+		symbols[symbol.lower()] = self
+		names[name.lower()]     = self
+		periods[period][group]  = self
+
+		self.symbol = symbol
+		self.name   = name
+
+	def __str__(self):
+		return self.symbol
